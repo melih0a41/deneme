15d1948ff96b19555c55201ff5063fa71fb5c251
melih0a41 <meliha411@gmail.com>
2025-06-24 07:12:36 +0300

    Delete gmodadminsuite-adminsits-2.0.2 directory

--- gmodadminsuite-adminsits-2.0.2/lua/autorun/server/!gas_adminsits_spt.lua
+++
@@ -1,9 +0,0 @@
-local entMeta = FindMetaTable("Entity")
-
-GAS_AdminSits_SetPreventTransmit = GAS_AdminSits_SetPreventTransmit or entMeta.SetPreventTransmit
-
-function entMeta:SetPreventTransmit(ply, preventTransmit)
-	if (hook.Run("GAS.AdminSits.TransmitStateChanged", self, ply, true) ~= false) then
-		return GAS_AdminSits_SetPreventTransmit(self, ply, preventTransmit)
-	end
-end
