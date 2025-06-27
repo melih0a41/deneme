8980ae91649495cf1597e6b9ee98f100b6aaee7e
melih0a41 <meliha411@gmail.com>
2025-06-27 10:15:46 +0300

    Add files via upload

---
+++ onyx_hud/lua/onyx/modules/hud/cfg/cl_level_systems.lua
@@ -0,0 +1,48 @@
+--[[
+
+Author: tochnonement
+Email: tochnonement@gmail.com
+
+21/08/2024
+
+--]]
+
+onyx.hud.RegisterLevelSystem( 'vrodankis', {
+    getLevel = function( ply )
+        return ( ply:getDarkRPVar( 'level' ) or 1 )
+    end,
+    getXP = function( ply )
+        return ( ply:getDarkRPVar( 'xp' ) or 0 )
+    end,
+    getMaxXP = function( ply )
+        -- omg...
+        -- this is from Vrondakis, there is no util func
+        local level = ( ply:getDarkRPVar( 'level' ) or 1 )
+        local maxXP = ( ( ( 10 + ( ( level * ( level + 1 ) * 90 ) ) ) ) * LevelSystemConfiguration.XPMult )
+        return maxXP
+    end,
+    customCheck = function()
+        return ( LevelSystemConfiguration ~= nil )
+    end,
+    onDetected = function()
+        hook.Remove( 'HUDPaint', 'manolis:MVLevels:HUDPaintA' )
+    end
+} )
+
+onyx.hud.RegisterLevelSystem( 'glorified', {
+    getLevel = function( ply )
+        return GlorifiedLeveling.GetPlayerLevel( ply )
+    end,
+    getXP = function( ply )
+        return GlorifiedLeveling.GetPlayerXP( ply )
+    end,
+    getMaxXP = function( ply )
+        return GlorifiedLeveling.GetPlayerMaxXP( ply )
+    end,
+    customCheck = function()
+        return ( GlorifiedLeveling ~= nil )
+    end,
+    onDetected = function()
+        hook.Remove( 'HUDPaint', 'GlorifiedLeveling.HUD.HUDPaint' )
+    end
+} )
