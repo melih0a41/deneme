a4d97afb871c54f2847b43f18e509a5f67f95f9f
melih0a41 <meliha411@gmail.com>
2025-07-03 02:02:30 +0300

    Delete sam_vip_system/lua/autorun directory

--- sam_vip_system/lua/autorun/server/vip_announcement_sv.lua
+++
@@ -1,9 +0,0 @@
-util.AddNetworkString("SAM.VIPAnnouncement")
-
-hook.Add("SAM.ChangedPlayerRank", "AnnounceVIPRank", function(ply, rank, oldRank)
-    if rank == "vip" and oldRank ~= "vip" then
-        net.Start("SAM.VIPAnnouncement")
-            net.WriteString(ply:Nick())
-        net.Broadcast()
-    end
-end)
