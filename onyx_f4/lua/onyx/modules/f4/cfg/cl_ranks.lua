c287e1adce5a7f8bd2b04d5a209b15949c12debb
melih0a41 <meliha411@gmail.com>
2025-07-04 10:19:13 +0300

    Add files via upload

---
+++ onyx_f4/lua/onyx/modules/f4/cfg/cl_ranks.lua
@@ -0,0 +1,21 @@
+--[[
+
+Contact: filipovskis@proton.me
+GitHub: https://github.com/filipovskis
+Date: 29/09/2024
+
+This code was written by Aleksandrs Filipovskis. Contact via email or GitHub.
+
+--]]
+
+--[[
+    **WARNING**
+    All defined ranks are considered admins.
+--]]
+
+onyx.f4.ranks = {}
+
+onyx.f4:DefineRank('superadmin', {
+    name = 'Owner',
+    color = Color(255, 71, 71)
+})
---
+++ onyx_f4/lua/onyx/modules/f4/languages/cl_polish.lua
@@ -0,0 +1,148 @@
+--[[
+Kayzed @ 2024
+--]]
+
+local LANG = {}
+
+-- TABS
+LANG.f4_jobs_u = 'PRACE'
+LANG.f4_jobs_desc = 'Wybierz swój zawód'
+
+LANG.f4_dashboard_u = 'PULPIT'
+LANG.f4_dashboard_desc = 'Ogólne informacje'
+
+LANG.f4_shop_u = 'SKLEP'
+LANG.f4_shop_desc = 'Zakup dowolne dobra'
+
+LANG.f4_admin_u = 'ADMIN'
+LANG.f4_admin_desc = 'Skonfiguruj dodatek'
+
+LANG.f4_donate_u = 'DONACJA'
+LANG.f4_donate_desc = 'Wspieraj serwer'
+
+LANG.addon_settings_u = 'USTAWIENIA'
+LANG.addon_settings_desc = 'Skonfiguruj dodatek'
+
+LANG.addon_stats_u = 'STATYSTYKI'
+LANG.addon_stats_desc = 'Sprawdź statystyki dodatku'
+
+LANG.addon_return_u = 'POWRÓT'
+LANG.addon_return_desc = 'Wróć do poprzedniej zakładki'
+
+-- Other
+LANG.f4_salary = 'Pensja'
+LANG.f4_price = 'Cena'
+LANG.f4_loading = 'Ładowanie'
+LANG.f4_purchases = 'Zakupy'
+LANG.f4_switches = 'Przełączniki'
+LANG.f4_unavailable = 'Niedostępne'
+LANG.f4_description_u = 'OPIS'
+LANG.f4_weapons_u = 'BROŃ'
+LANG.f4_entities_u = 'BYTY'
+LANG.f4_ammo_u = 'AMUNICJA'
+LANG.f4_food_u = 'JEDZENIE'
+LANG.f4_shipments_u = 'PACZKI'
+LANG.f4_become_u = 'ZOSTAŃ'
+LANG.f4_create_vote_u = 'UTWÓRZ GŁOSOWANIE'
+LANG.f4_general_u = 'OGÓLNE'
+LANG.f4_police_u = 'POLICJA'
+LANG.f4_mayor_u = 'BURMISTRZ'
+LANG.f4_confirm_u = 'POTWIERDŹ'
+LANG.f4_cancel_u = 'ANULUJ'
+LANG.f4_mostpopular_u = 'NAJPOPULARNIEJSZE'
+LANG.f4_chart_u = 'WYKRES'
+LANG.f4_loading_u = 'ŁADOWANIE'
+LANG.f4_empty_u = 'PUSTE'
+LANG.f4_favorite_u = 'ULUBIONE'
+
+LANG.f4_playersonline_u = 'GRACZE ONLINE'
+LANG.f4_totalmoney_u = 'CAŁKOWITA KASA'
+LANG.f4_staffonline_u = 'ADMINISTRACJA ONLINE'
+LANG.f4_actions_u = 'AKCJE'
+
+LANG.f4_show_favorite = 'Pokaż Ulubione'
+
+LANG.requires_level = 'Wymaga Poziomu {level}'
+
+-- Actions
+LANG['f4_action_input_amount'] = 'Wprowadź ilość'
+LANG['f4_action_input_text'] = 'Wprowadź tekst'
+LANG['f4_action_input_reason'] = 'Podaj powód'
+LANG['f4_action_choose_player'] = 'Wybierz gracza'
+
+LANG['f4_action_confirm_action'] = 'Potwierdź akcję'
+LANG['f4_action_drop_money'] = 'Upuść Pieniądze'
+LANG['f4_action_give_money'] = 'Daj Pieniądze'
+LANG['f4_action_change_name'] = 'Zmień Imię'
+LANG['f4_action_drop_weapon'] = 'Upuść Broń'
+LANG['f4_action_sell_doors'] = 'Sprzedaj Wszystkie Drzwi'
+
+LANG['f4_action_warrant'] = 'Wystaw Nakaz'
+LANG['f4_action_wanted'] = 'Oznacz jako Poszukiwany'
+
+LANG['f4_toggle_lockdown'] = 'Przełącz Blokadę'
+LANG['f4_give_license'] = 'Daj Licencję'
+
+-- Phrases
+LANG['f4_search_text'] = 'Szukaj po imieniu...'
+
+-- Settings
+LANG['f4.option_url_desc'] = 'Podaj URL (pozostaw puste, aby wyłączyć)'
+
+LANG['f4.discord_url.name'] = 'Discord'
+LANG['f4.discord_url.desc'] = 'Dołącz do naszego serwera Discord'
+
+LANG['f4.forum_url.name'] = 'Forum'
+LANG['f4.forum_url.desc'] = 'Poznaj naszą społeczność'
+
+LANG['f4.steam_url.name'] = 'Steam'
+LANG['f4.steam_url.desc'] = 'Dołącz do naszej grupy Steam'
+
+LANG['f4.rules_url.name'] = 'Zasady'
+LANG['f4.rules_url.desc'] = 'Poznaj zasady'
+
+LANG['f4.donate_url.name'] = 'Donacja'
+
+LANG['f4.website_ingame.name'] = 'Przeglądarka'
+LANG['f4.website_ingame.desc'] = 'Użyj przeglądarki w grze, aby otworzyć URL strony'
+
+LANG['f4.title.name'] = 'Tytuł'
+LANG['f4.title.desc'] = 'Tytuł dla ramki'
+
+LANG['f4.hide_donate_tab.name'] = 'Ukryj zakładkę Donacja'
+LANG['f4.hide_donate_tab.desc'] = 'Ukryj zakładkę integracji sklepu kredytowego'
+
+LANG['f4.edit_job_colors.name'] = 'Modyfikuj Kolory Prac'
+LANG['f4.edit_job_colors.desc'] = 'Czy kolory prac mają być jaśniejsze'
+
+LANG['f4.hide_admins.name'] = 'Ukryj Sekcję Administratorów'
+LANG['f4.hide_admins.desc'] = 'Ukryj sekcję listy adminów na pulpicie'
+
+LANG['f4.admin_on_duty.name'] = 'Włączona Praca Administratora'
+LANG['f4.admin_on_duty.desc'] = 'Wyświetlaj jako administrator tylko osoby z określoną pracą'
+
+LANG['f4.admin_on_duty_job.name'] = 'Nazwa Pracy Administratora'
+LANG['f4.admin_on_duty_job.desc'] = 'Nazwa pracy administratora*'
+
+LANG['f4.colored_items.name'] = 'Kolorowy Gradient'
+LANG['f4.colored_items.desc'] = 'Włącz delikatny gradient dla przedmiotów/prac'
+
+LANG['f4.item_columns.name'] = 'Kolumny'
+LANG['f4.item_columns.desc'] = 'Liczba kolumn dla Przedmiotów'
+
+LANG['f4.job_columns.name'] = 'Kolumny'
+LANG['f4.job_columns.desc'] = 'Liczba kolumn dla Prac'
+
+LANG['f4.model_3d.name'] = 'Modele 3D'
+LANG['f4.model_3d.desc'] = 'Włącz renderowanie w czasie rzeczywistym dla ikon Przedmiotów/Prac'
+
+LANG['f4.item_show_unavailable.name'] = 'Niedostępne Przedmioty'
+LANG['f4.item_show_unavailable.desc'] = 'Pokaż przedmioty, które nie przeszły customCheck'
+
+LANG['f4.job_show_unavailable.name'] = 'Niedostępne Prace'
+LANG['f4.job_show_unavailable.desc'] = 'Pokaż prace, które nie przeszły customCheck'
+
+LANG['f4.job_show_requirejob.name'] = 'Zależne Prace'
+LANG['f4.job_show_requirejob.desc'] = 'Pokaż prace, których nie można wybrać z powodu niewłaściwej pracy gracza'
+
+onyx.lang:AddPhrases('polish', LANG)
