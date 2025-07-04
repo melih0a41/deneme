1722b9902d06d4efbc1c791a8c0fe1b2185960d1
melih0a41 <meliha411@gmail.com>
2025-07-04 10:33:21 +0300

    Add files via upload

---
+++ onyx_scoreboard/lua/onyx/modules/scoreboard/languages/cl_polish.lua
@@ -0,0 +1,79 @@
+--[[
+KayZed @ 2024
+--]]
+
+local LANG = {}
+
+-- Phrases
+LANG['copied_clipboard'] = 'Skopiowano do schowka'
+LANG['scoreboard_search'] = 'Szukaj... (Nazwa/SteamID)'
+LANG['you'] = 'Ty'
+LANG['friend'] = 'Przyjaciel'
+LANG['addon_return_u'] = 'POWRÓT'
+
+-- Columns
+LANG['scoreboard_col_team'] = 'Drużyna'
+LANG['scoreboard_col_job'] = 'Praca'
+LANG['scoreboard_col_money'] = 'Pieniądze'
+LANG['scoreboard_col_rank'] = 'Ranga'
+LANG['scoreboard_col_karma'] = 'Karma'
+LANG['scoreboard_col_playtime'] = 'Czas Gry'
+LANG['scoreboard_col_health'] = 'Zdrowie'
+LANG['scoreboard_col_level'] = 'Poziom'
+LANG['scoreboard_col_none'] = 'Brak'
+LANG['scoreboard_col_gang'] = 'Gang'
+LANG['scoreboard_col_ashop_badges'] = 'Odznaki'
+LANG['scoreboard_col_faction'] = 'Frakcja'
+
+-- Name Effects
+LANG['scoreboard_eff_default'] = 'Domyślny'
+LANG['scoreboard_eff_glow'] = 'Poświata'
+LANG['scoreboard_eff_rainbow'] = 'Tęcza'
+LANG['scoreboard_eff_scanning_vertical'] = 'Skanujący (Pionowo)'
+LANG['scoreboard_eff_scanning_horizontal'] = 'Skanujący (Poziomo)'
+LANG['scoreboard_eff_gradient_invert'] = 'Gradient (Odwróć Kolor)'
+LANG['scoreboard_eff_wavy_dual'] = 'Falujący (Podwójny Kolor)'
+
+-- Buttons
+LANG['scoreboard_btn_profile'] = 'Otwórz Profil'
+LANG['scoreboard_btn_freeze'] = 'Zamroź'
+LANG['scoreboard_btn_goto'] = 'Idź Do'
+LANG['scoreboard_btn_bring'] = 'Przynieś'
+LANG['scoreboard_btn_return'] = 'Powrót'
+LANG['scoreboard_btn_respawn'] = 'Odrodź'
+LANG['scoreboard_btn_slay'] = 'Zabij'
+LANG['scoreboard_btn_spectate'] = 'Obserwuj'
+
+-- Words
+LANG['rank_id'] = 'Identyfikator Rangi'
+LANG['name'] = 'Nazwa'
+LANG['effect'] = 'Efekt'
+LANG['color'] = 'Kolor'
+LANG['preview'] = 'Podgląd'
+LANG['creation'] = 'Tworzenie'
+LANG['save'] = 'Zapisz'
+LANG['dead'] = 'Martwy'
+LANG['create_new'] = 'Utwórz Nowy'
+LANG['column'] = 'Kolumna'
+
+-- Settings
+LANG['addon_settings_u'] = 'USTAWIENIA'
+LANG['scoreboard_ranks_u'] = 'RANGI'
+LANG['scoreboard_columns_u'] = 'KOLUMNY'
+
+LANG['scoreboard.title.name'] = 'Tytuł'
+LANG['scoreboard.title.desc'] = 'Wprowadź tytuł dla ramki'
+
+LANG['scoreboard.group_teams.name'] = 'Grupuj Zespoły'
+LANG['scoreboard.group_teams.desc'] = '(DarkRP) Grupuj zespoły według kategorii prac'
+
+LANG['scoreboard.colored_players.name'] = 'Koloryzowany Gradient'
+LANG['scoreboard.colored_players.desc'] = 'Pokaż gradient kolorów na linii gracza'
+
+LANG['scoreboard.blur.name'] = 'Rozmycie Motywu'
+LANG['scoreboard.blur.desc'] = 'Włącz motyw rozmycia'
+
+LANG['scoreboard.scale.name'] = 'Skala Wielkości Ramki'
+LANG['scoreboard.scale.desc'] = 'Skaluj wielkość ramki tabeli wyników'
+
+onyx.lang:AddPhrases('polish', LANG)
