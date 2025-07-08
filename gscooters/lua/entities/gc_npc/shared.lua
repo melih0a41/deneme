f994189e1a3d642ba983992e503a98325886a264
melih0a41 <meliha411@gmail.com>
2025-07-08 17:29:11 +0300

    Delete gscooters/lua directory

--- gscooters/lua/entities/gc_npc/shared.lua
+++
@@ -1,7 +0,0 @@
-ENT.Type 			= "ai"
-ENT.Base 			= "base_ai"
-ENT.PrintName		= "Scooter Retriever"
-ENT.Category		= "gScooters"
-ENT.Author			= "painless"
-ENT.Purpose			= "Do people even look at this? Well... it's an NPC."
-ENT.Spawnable       = false
