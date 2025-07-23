8414393ad74607bf74aab90e89f8a679b1b1547f
melih0a41 <meliha411@gmail.com>
2025-07-23 19:42:25 +0300

    Delete tabdetection directory

--- tabdetection/lua/autorun/tabdetection_init.lua
+++
@@ -1,9 +0,0 @@
-
-if SERVER then
-    AddCSLuaFile("tabdetection/cl_tabdetection.lua")
-    AddCSLuaFile("tabdetection/cl_overhead_display.lua")
-    include("tabdetection/sv_tabdetection.lua")
-else
-    include("tabdetection/cl_tabdetection.lua")
-    include("tabdetection/cl_overhead_display.lua")
-end
