d5ef8432627fa0369eb5b2a604999cc46f88b4cd
melih0a41 <meliha411@gmail.com>
2025-08-06 03:12:17 +0300

    Delete bricks-enchanced-sweps/lua directory

--- bricks-enchanced-sweps/lua/bricksenchancedsweps/bes_shared.lua
+++
@@ -1,9 +0,0 @@
-BES.FText = {}
-BES.FText.Nothing = {
-    "Nothing to see here...",
-    "uWu whats this?"
-}
-
-hook.Add( "Initialize", "BES_Initialize_SWEPs", function()
-    table.insert( GAMEMODE.Config.DefaultWeapons, "dsr_keys" )
-end )
