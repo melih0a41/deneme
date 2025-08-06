f8757ad24611d2d07688cbc1fb59d222ada639ad
melih0a41 <meliha411@gmail.com>
2025-08-06 03:14:01 +0300

    Delete tbfy_shared_v2 directory

--- tbfy_shared_v2/lua/tbfy_shared/modules/hooks/cl_hooks.lua
+++
@@ -1,6 +0,0 @@
-
-hook.Add("PostPlayerDraw", "TBFY_PostPlayerDraw", function(Player)
-	if LocalPlayer() != Player then
-		TBFY_SH:DrawPlayerEquips(Player)
-	end
-end)
--- tbfy_shared_v2/lua/tbfy_shared/modules/software_archives/sh_archive.lua
+++
@@ -1,5 +0,0 @@
-TBFY_SH.GArchiveTypes = TBFY_SH.GArchiveTypes or {}
-
-function TBFY_SH:RegisterGArchive(Table)
-	TBFY_SH.GArchiveTypes[Table.ID] = Table
-end
