f28afa39730c37de8c391a21463c53446ac4872d
melih0a41 <meliha411@gmail.com>
2025-08-13 00:24:24 +0300

    Delete voidcases-2.9.6/lua directory

--- voidcases-2.9.6/lua/voidcases/sv_menu.lua
+++
@@ -1,9 +0,0 @@
-util.AddNetworkString("VoidCases_NotifyPlayer")
-
-
-hook.Add("PlayerSay", "VoidCases.PlayerCommand", function (ply, text)
-    if (!VoidCases.HasTLoaded) then return end
-    if (string.lower(text) == VoidCases.Config.MenuCommand or string.lower(text) == "!voidcases") then
-        ply:ConCommand("voidcases")
-    end
-end)
