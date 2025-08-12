a4fd795f7c21ddefa2940cc0a8f551b2a01647f6
melih0a41 <meliha411@gmail.com>
2025-08-13 00:23:47 +0300

    Delete pvault/lua directory

--- pvault/lua/entities/pvault_mask/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-ENT.PrintName = "Mask"
-ENT.Author = "Owain Owjo & The One Free-Man"
-ENT.Category = "pVault"
-ENT.RenderGroup = RENDERGROUP_BOTH
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
